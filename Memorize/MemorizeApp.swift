90103ef7f515b983da259620599c8a3264ab31a0
Stephanie Salgado <alwaysforevergirl@outlook.com>
2024-05-29 17:25:44 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Stephanie Salgado on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
